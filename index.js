94cda96a3b199eaa57293c3345180b4135fa610f
Amanda Steckler <amandajsteckler@gmail.com>
2017-01-26 12:16:03 -0500

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 window.addEventListener("load", false);
 
 function newGame() {
+  // create array of all the table rows' contents
   var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""]
+
+  // shuffle nums
   for (var i = nums.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var k = nums[i];
@@ -9,9 +12,11 @@ function newGame() {
         nums[j] = k;
     }
 
+  // iterate through table rows, set contents equal to shuffled nums
+  // set class for "" element to "unfilled", otherwise to "filled"
   for (var p=0; p < nums.length; p++) {
     currentElement = document.getElementById((p + 1).toString());
-    currentElement.innerHTML = nums[p]
+    currentElement.innerHTML = nums[p];
     if (currentElement.innerHTML === "") {
       currentElement.className = "unfilled"
     } else {
@@ -19,27 +24,36 @@ function newGame() {
     }
   }
 
+  // declare variable to set up winning status
   var won = false
 
 }
 
 function clicked(block) {
+  // get clicked block id
   blockId = parseInt(block.id)
 
+  // block on top will be 4 less
   blockOnTopId = blockId - 4
   blockOnTop = document.getElementById(blockOnTopId.toString())
 
+  // block on bottom will be 4 more
   blockOnBottomId = blockId + 4
   blockOnBottom = document.getElementById(blockOnBottomId.toString())
 
+  // block to the left will be one less
   blockToTheLeftId = blockId - 1
   blockToTheLeft = document.getElementById(blockToTheLeftId.toString())
 
+  // block to the right will be one more
   blockToTheRightId = blockId + 1
   blockToTheRight = document.getElementById(blockToTheRightId.toString())
 
   // come back to the issue where 5-1 = 4 for example
 
+  // if the block on the bottom exists and is empty
+  // switch the block's and the bottom block's content/class
+  // repeat for each possible move
   if (blockOnBottom != null && blockOnBottom.innerHTML === "") {
     blockOnBottom.innerHTML = block.innerHTML
     blockOnBottom.className = "filled"
@@ -62,20 +76,23 @@ function clicked(block) {
     block.className = "unfilled"
   }
 
-  var ordered = []
+  // iterate through table rows, if id & content match, push true, otherwise, push false
+  var matchingIdAndContent = []
   for (var i=1; i < 16; i++) {
     var el = document.getElementById(i.toString())
     if (el.id === el.innerHTML) {
-      ordered.push(true)
+      matchingIdAndContent.push(true)
     } else {
-      ordered.push(false)
+      matchingIdAndContent.push(false)
     }
   }
 
-  if (ordered.indexOf(false) < 0) {
+  // if a false exists in the matchingIdAndContent array, user hasn't won yet
+  if (matchingIdAndContent.indexOf(false) < 0) {
     won = true
   }
 
+  // if user won, add winning message to <div id="won"></div>
   if (won === true) {
     wonDiv = document.getElementById("won")
     wonDiv.innerHTML = "You won!"
